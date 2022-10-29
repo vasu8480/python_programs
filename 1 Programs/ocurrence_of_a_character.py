ce9fff1ecd2442c677e7523c82579acfd18b7e1c
vasu8480 <vasu8480@gmail.com>
2022-10-29 16:45:30 +0530

    programs

---
+++ 1 Programs/ocurrence_of_a_character.py
@@ -0,0 +1,9 @@
+str1="vasu is a good a00 boy"
+str2="a"
+c=0
+for i in str1:
+	if i==str2:
+		c+=1
+print(c)
+----------------------------------------------------------------------------------
+print(str1.count("a"))
