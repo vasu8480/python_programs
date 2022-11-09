56b94d811176554d4ec1e65b899058256c237cba
vasu8480 <vasu8480@gmail.com>
2022-11-09 19:45:45 +0530

    Programs

--- 1 Programs/patterns.py
+++ 1 Programs/patterns.py
@@ -1,57 +1,62 @@
-#patterns
-'''
-*
-**   
-***  
-**** 
-*****
+# #patterns
 # '''
-for i in range(0,5):
-	for j in range(0,i+1):
-		print("*",end="")
-	print()
+# *
+# **   
+# ***  
+# **** 
+# *****
+# # '''
+# for i in range(0,5):
+# 	for j in range(0,i+1):
+# 		print("*",end="")
+# 	print()
 
-myList = []
-for i in range(1,5+1):
-    myList.append("*"*i)
-print("\n".join(myList))
+# myList = []
+# for i in range(1,5+1):
+#     myList.append("*"*i)
+# print(myList)
+# print(type(myList))
+# print("\n".join(myList))
+# print(type("\n".join(myList)))
 
-'''
-	  *
-   **
-  ***
- ****
- '''
-for row in range(0, 5):
-    print(" " * (5 - row) +"*" * row)
+# '''
+# 	  *
+#    **
+#   ***
+#  ****
+#  '''
+# for row in range(1, 5):
+#     print(" " * (5 - row-1) + "*" * row)
 		
 
-rows = 5
-b = 0
-# reverse for loop from 5 to 0
-"""
-1 1 1 1 1 
-2 2 2 2 
-3 3 3 
-4 4 
-5 
-"""
-for i in range(rows, 0, -1):
-    b += 1
-    for j in range(1, i + 1):
-        print(b, end=' ')
-    print('')
-'''
-* * * * *
-* * * *
-* * *
-* *
-*
-'''
-rows = 5
-for i in range(rows + 1, 0, -1):
-    # nested reverse loop
-    for j in range(0, i - 1):
-        # display star
-        print("*", end=' ')
-    print(" ")
+
+# # reverse for loop from 5 to 0
+# """
+# 1 1 1 1 1 
+# 2 2 2 2 
+# 3 3 3 
+# 4 4 
+# 5 
+# """
+
+
+# b = 5
+# for i in range(6, 0, -1):
+#     b += 1
+#     for j in range(0, i ):
+#         print(b, end=' ')
+#     print()
+
+# '''
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# '''
+
+
+# for i in range(6, 0, -1):
+#     for j in range(0, i - 1):
+#         print("*", end=' ')
+#     print(" ")
--- 1 Programs/sum_of_n_natural_number.py
+++ 1 Programs/sum_of_n_natural_number.py
@@ -3,4 +3,3 @@ sum=0
 for i in range(1,n+1):
 		sum+=i
 print(sum)
-
