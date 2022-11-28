653e2e8550e7173375704d3c333f7d8ad6a8c743
Vasu Nurukurthi <Vasu8480@gmail.com>
2022-11-28 11:57:57 +0530

    pali

--- 1 Programs/str_pali.py
+++ 1 Programs/str_pali.py
@@ -1,3 +1,8 @@
+def ispalindromic (s) :
+# Note that s[-jJ for i in [0,len(s) - 1] is st-(i + t)l
+	return all(s[i] == s[~i] for i in range(len(s) // 2))
+print(ispalindromic("madam"))
+#-------------------- Method-2 -----------------------------
 str="Madam"
 str=str.casefold()
 rev=reversed(str)
@@ -5,7 +10,7 @@ if (list(str)==list(rev)):
 	print('pali')
 else:
 	print("not pali")
-#-------------------- Method-2 -----------------------------
+#-------------------- Method-3 -----------------------------
 s=""
 for i in str:
 	s=i+s
@@ -13,7 +18,7 @@ if s==str:
 	print('pali')
 else:
 	print("not pali")
-#-------------------- Method-3------------------------------
+#-------------------- Method-4------------------------------
 string = list(str)
 string.reverse()
 print("".join(string))
@@ -21,7 +26,7 @@ if s==str:
 	print('pali')
 else:
 	print("not pali")
-#-------------------- Method-------------------------------
+#-------------------- Method-5 ------------------------------
 string = [str[i] for i in range(len(str)-1, -1, -1)]
 print("".join(string))
 if s==str:
